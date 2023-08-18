0d612a3eedf8b1f59fca9b6d17064094c073ed8a
Kevin <kevin.macias.a@gmail.com>
2023-08-18 11:04:31 -0700

    [UPDATE] main.ts to test added functionalities

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
+import Game from './Game'
 import populateGrid from './populateGrid'
 import './style.css'
 
 const grid = document.getElementById("game-grid")
-if (!grid) console.error("Game grid is missing")
-else populateGrid(grid)
+if (!grid) throw new Error("Grid is missing")
+populateGrid(grid)
+
+const game = new Game()
+game.render()
+
+const keydownHandler = (event: KeyboardEvent) => {
+  const button = event.key
+  console.log(button)
+  if (button === "s") {
+    game.move(0, 1)
+  }
+}
+document.addEventListener("keydown", keydownHandler)
