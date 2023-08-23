969c85c024c47793ffa7fada543e00ad3478c29b
Kevin <kevin.macias.a@gmail.com>
2023-08-22 21:20:22 -0700

    [UPDATE] change ZBlock.ts render color

--- src/Block/ZBlock.ts
+++ src/Block/ZBlock.ts
@@ -7,7 +7,7 @@ class ZBlock extends Block {
       new Point(0, 0), new Point(1, 0),
       /* z shape space */ new Point(1, 1), new Point(2, 1)
     ]
-    super(new Point(x, y), shape, "Plum")
+    super(new Point(x, y), shape, "HotPink")
   }
 
 }
