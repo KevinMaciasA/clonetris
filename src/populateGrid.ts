1d932e425ea329d30cc3f2ee8be6995d455e98f4
Kevin <kevin.macias.a@gmail.com>
2023-08-22 21:16:37 -0700

    [UPDATE] update populateGrid.ts to work with Grid class & also add custom attr to all elements

--- src/populateGrid.ts
+++ src/populateGrid.ts
@@ -1,11 +1,9 @@
-export default function populateGrid(grid: HTMLElement) {
-  const columns = 10
-  const rows = 20
-
+export default function populateGrid(grid: HTMLElement, rows: number, columns: number) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       const div = document.createElement("div")
       div.classList.add("grid-item")
+      div.dataset.isBlocked = "false"
       div.id = `${j}-${i}`
       // div.innerText = `${j}-${i}` x-y
       grid.appendChild(div)
