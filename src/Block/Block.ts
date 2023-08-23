f96ba1cb77e1f7b4e96993287b4df679ca6eba39
Kevin <kevin.macias.a@gmail.com>
2023-08-22 21:23:16 -0700

    [UPDATE] Block.ts, rename position -> point, added isBlocked custom attr, & position utils

--- src/Block/Block.ts
+++ src/Block/Block.ts
@@ -1,43 +1,55 @@
 import Point from "../utils/Point";
+import { addCustomAttr } from "../utils/addCustomAttr";
 import { colorPoint, ColorName } from "../utils/colorPoint";
 
 abstract class Block {
-  position: Point
+  point: Point
   protected shape: Point[]
   protected color: ColorName
 
   constructor(initPosition: Point, shape: Point[], color: ColorName = "Black") {
-    this.position = initPosition
+    this.point = initPosition
     this.shape = shape
     this.color = color
   }
 
   draw() {
     this.shape.forEach((point) => {
-      const p = this.position.add(point)
+      const p = this.point.add(point)
       colorPoint(this.color, p)
+      addCustomAttr("isBlocked", true, p)
     })
   }
 
   clear() {
     this.shape.forEach((point) => {
-      const p = this.position.add(point)
+      const p = this.point.add(point)
       colorPoint("White", p)
+      addCustomAttr("isBlocked", false, p)
     })
   }
 
   //absolute movement
   absMove(position: Point) {
     this.clear()
-    this.position = position
+    this.point = position
     this.draw()
   }
   // relative movement
   move(x: number, y: number) {
     this.clear()
-    this.position.x += x
-    this.position.y += y
+    this.point.x += x
+    this.point.y += y
     this.draw()
+  }
+
+  position(): Point[] {
+    return this.shape.map(point => this.point.add(point))
+  }
+
+  previewMove(x: number, y: number): Point[] {
+    const newPosition = this.point.add(new Point(x, y))
+    return this.shape.map(point => newPosition.add(point))
   }
 }
 
