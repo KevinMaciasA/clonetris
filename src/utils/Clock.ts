68029d8ee04595fe63c4258e15182c47a438e37d
Kevin <kevin.macias.a@gmail.com>
2023-09-14 13:39:38 -0700

    [UPDATE] Name Interval.ts to Clock.ts

--- src/utils/Clock.ts
+++ src/utils/Clock.ts
@@ -1,20 +1,40 @@
-class Interval {
+class Clock {
+  private isRunning: boolean;
   private callback: () => void;
   private miliSeconds: number
-  private intervalId: number;
+  private intervalId: number | null;
 
   constructor(callback: () => void, ms: number) {
+    this.isRunning = false
     this.callback = callback
     this.miliSeconds = ms
+    this.intervalId = null;
+  }
+
+  start() {
+    if (this.isRunning) return
+
     this.intervalId = window.setInterval(this.callback, this.miliSeconds)
+    this.isRunning = true;
+  }
+
+  stop() {
+    if (!this.isRunning || this.intervalId === null) return
+
+    window.clearInterval(this.intervalId)
+    this.isRunning = false;
   }
 
   reset() {
+    if (!this.isRunning || this.intervalId === null) return
+
     window.clearInterval(this.intervalId)
     this.intervalId = window.setInterval(this.callback, this.miliSeconds)
   }
 
   change(ms: number) {
+    if (!this.isRunning || this.intervalId === null) return
+
     window.clearInterval(this.intervalId)
     this.intervalId = window.setInterval(this.callback, ms)
   }
@@ -27,4 +47,4 @@ class Interval {
   }
 }
 
-export default Interval
+export default Clock
